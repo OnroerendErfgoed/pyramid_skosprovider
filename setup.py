6aa7d8274fadf57c8ce2ebc3716d58c68a4a62a7
Koen Van Daele <koen_van_daele@telenet.be>
2015-02-04 13:28:56 +0100

    Fixen op skosprovider > 0.5.0 zodat tox terug werkt.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ CHANGES = open(os.path.join(here, 'HISTORY.rst')).read()
 
 requires = [
     'pyramid',
-    'skosprovider>=0.4.2'
+    'skosprovider>0.5.0'
 ]
 
 tests_requires = [
@@ -21,7 +21,7 @@ testing_extras = tests_requires + []
 
 setup(
     name='pyramid_skosprovider',
-    version='0.5.0',
+    version='0.6.0-dev',
     license='MIT',
     description='Integration of skosprovider in pyramid',
     long_description=README + '\n\n' + CHANGES,
