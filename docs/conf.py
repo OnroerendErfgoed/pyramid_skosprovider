f9e597107ce4a9fd2c453e288b76c3deda47c44b
Koen Van Daele <koen_van_daele@telenet.be>
2020-07-20 16:53:25 +0200

    Update docs and project housekeeping. Refs #72

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Pyramid Skosprovider'
-copyright = u'2013-2017, Koen Van Daele'
+copyright = u'2013-2020, Koen Van Daele'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
