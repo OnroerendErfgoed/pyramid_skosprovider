c13f4692f110154e77a3c2b74d4eb3722cec991a
Wim De Clercq <4599667+Wim-De-Clercq@users.noreply.github.com>
2025-12-11 12:35:45 +0100

    Make requirements file per python version.

---
+++ scripts/compile-requirements.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR/.."
+
+BASE_PIP_COMPILE_ARGS="-q --no-header --strip-extras --resolver=backtracking --no-emit-options --no-emit-trusted-host --no-emit-find-links"
+PYTHON_VERSIONS="3.10 3.11 3.12 3.13 3.14"
+
+for version in $PYTHON_VERSIONS; do
+    PIP_COMPILE_ARGS="$BASE_PIP_COMPILE_ARGS --python-version ${version}"
+    echo "Compiling $version requirements$version.txt..."
+    uv pip compile $PIP_COMPILE_ARGS -o "$SCRIPT_DIR/../lockfiles/requirements$version.txt" pyproject.toml
+    echo " └Done"
+
+    echo "Compiling $version requirements-dev$version.txt..."
+    uv pip compile $PIP_COMPILE_ARGS --all-extras -o "$SCRIPT_DIR/../lockfiles/requirements-dev$version.txt" pyproject.toml
+    echo " └Done"
+done
+
+cd -
