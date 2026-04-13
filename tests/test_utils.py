e62a1afd3ee53a5465a45bdc82a5cbd147ef5c25
TimVanCampenhout <tim.vancampenhout@vlaanderen.be>
2026-04-13 11:04:54 +0200

    Use proper linting tools
    Introduce ruff with pre-commit configuration. Remove flake8.
    
    Fixes #138

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,34 +2,18 @@
 
 from pyramid import testing
 
-from .fixtures.data import (
-    larch,
-    chestnut,
-    species,
-    trees
-)
-
-from skosprovider.skos import (
-    Concept,
-    Collection,
-    Label,
-    Source
-)
-
-import json
 
 import unittest
-from unittest.mock import Mock
 
 
 class TestQueryBuilder:
-
     def _get_dummy_request(self, *args, **kwargs):
         request = testing.DummyRequest(*args, **kwargs)
         return request
 
     def _get_fut(self, request=None, postprocess=False):
         from pyramid_skosprovider.utils import QueryBuilder
+
         if not request:
             request = self._get_dummy_request()
         qb = QueryBuilder(request, postprocess)
@@ -57,10 +41,9 @@ class TestQueryBuilder:
         assert 'type' not in q
 
     def test_build_label_dfs_with_star(self):
-        request = self._get_dummy_request({
-            'mode': 'dijitFilteringSelect',
-            'label': 'Di*'
-        })
+        request = self._get_dummy_request(
+            {'mode': 'dijitFilteringSelect', 'label': 'Di*'}
+        )
         qb = self._get_fut(request)
         q = qb()
         assert isinstance(q, dict)
@@ -68,9 +51,7 @@ class TestQueryBuilder:
         assert q['label'] == 'Di'
 
     def test_build_collection(self):
-        request = self._get_dummy_request({
-            'collection': 3
-        })
+        request = self._get_dummy_request({'collection': 3})
         qb = self._get_fut(request)
         q = qb()
         assert isinstance(q, dict)
@@ -78,9 +59,7 @@ class TestQueryBuilder:
         assert q['collection'] == {'id': 3, 'depth': 'all'}
 
     def test_build_matches(self):
-        request = self._get_dummy_request({
-            'match': 'https://thingy.org/thing'
-        })
+        request = self._get_dummy_request({'match': 'https://thingy.org/thing'})
         qb = self._get_fut(request)
         q = qb()
         assert isinstance(q, dict)
@@ -88,48 +67,27 @@ class TestQueryBuilder:
         assert q['matches'] == {'uri': 'https://thingy.org/thing'}
 
     def test_build_matches_broader(self):
-        request = self._get_dummy_request({
-            'match': 'https://thingy.org/thing',
-            'match_type': 'exact'
-        })
+        request = self._get_dummy_request(
+            {'match': 'https://thingy.org/thing', 'match_type': 'exact'}
+        )
         qb = self._get_fut(request)
         q = qb()
         assert isinstance(q, dict)
         assert 'matches' in q
-        assert q['matches'] == {
-            'uri': 'https://thingy.org/thing',
-            'type': 'exact'
-        }
+        assert q['matches'] == {'uri': 'https://thingy.org/thing', 'type': 'exact'}
+
 
 class TestRangeHeaders(unittest.TestCase):
-
     def test_parse_range_header(self):
         from pyramid_skosprovider.utils import parse_range_header
+
         headers = [
-            {
-                'header': 'items=0-19',
-                'result': {
-                    'start': 0,
-                    'number': 20,
-                    'finish': 19
-                }
-            }, {
-                'header': 'items:0-19',
-                'result': False
-            }, {
-                'header': 'test',
-                'result': False
-            }, {
-                'header': 'items=t-t',
-                'result': False
-            }, {
-                'header': 'items=10-0',
-                'result': {
-                    'start': 10,
-                    'finish': 10,
-                    'number': 1
-                }
-            }]
+            {'header': 'items=0-19', 'result': {'start': 0, 'number': 20, 'finish': 19}},
+            {'header': 'items:0-19', 'result': False},
+            {'header': 'test', 'result': False},
+            {'header': 'items=t-t', 'result': False},
+            {'header': 'items=10-0', 'result': {'start': 10, 'finish': 10, 'number': 1}},
+        ]
         for header in headers:
             res = parse_range_header(header['header'])
             self.assertEqual(res, header['result'])
